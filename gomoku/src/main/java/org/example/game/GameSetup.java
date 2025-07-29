6f795cf22dc707332a11a618cb118428749bfb8f
madinabolat <madinabolat01@gmail.com>
2025-07-28 21:58:47 -0400

    config

---
+++ gomoku/src/main/java/org/example/game/GameSetup.java
@@ -0,0 +1,7 @@
+package org.example.game;
+
+public class GameSetup {
+    //create players - ask if human, ai or smart ai. if human -> get names
+    //create board
+    //get num of consecutive cells to win
+}
