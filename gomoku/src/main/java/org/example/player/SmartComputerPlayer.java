01b8c36f4279a709a01cbf6c5b86811e2c1eba32
madinabolat <madinabolat01@gmail.com>
2026-04-11 21:29:29 -0400

    fix smartcomputer logic

--- gomoku/src/main/java/org/example/player/SmartComputerPlayer.java
+++ gomoku/src/main/java/org/example/player/SmartComputerPlayer.java
@@ -4,11 +4,9 @@ import org.example.board.Board;
 import org.example.board.CellState;
 import org.example.game.WinChecker;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
-import java.util.UUID;
-
 
 public class SmartComputerPlayer extends Player{
     WinChecker winChecker;
@@ -18,48 +16,61 @@ public class SmartComputerPlayer extends Player{
         this.winChecker = winChecker;
     }
 
-    @Override
-    public int[] generateCoordinates(Board board){
-        System.out.println("Computer " + name + " choosing its move:");
-        int x = -1;
-        int y = -1;
+    public int[] getCellWithHighestConsecutiveLength(Board board, CellState cellState){
         CellState[][] boardArray = board.board;
-
-        //scan the board first
-        //get the opponent's cells, my  cells. where i should put.
-        //if i can win -> win. if not, find opponent. if they can win -> block. 
+        ArrayList<int[]> occupiedCellsOfOneType = new ArrayList<>();
         for (int i = 0; i < board.boardSize; i++) {
             for (int j = 0; j < board.boardSize; j++) {
-                if (boardArray[i][j] != CellState.EMPTY && boardArray[i][j] != cellState) {
-                    int max = winChecker.maxConsecutiveLengthWithDirections(i, j)[0];
-                    int max_dx = winChecker.maxConsecutiveLengthWithDirections(i, j)[1];
-                    int max_dy = winChecker.maxConsecutiveLengthWithDirections(i, j)[2];
-
-                    if (max >= winChecker.numberOfConsecutiveCellsToWin-1) {
-                        x = i + (max-1) * max_dx;
-                        y = j + (max-1) * max_dy;
-                        continue;
-                    }
+                if (boardArray[i][j] == cellState){
+                    occupiedCellsOfOneType.add(new int[]{i,j});
                 }
+            }
+        }
+        int maxConsecutiveLength = 0;
+        int[] cellCoordinatesWithMaxConsecutiveLength = new int[2];
+        for (int[] coordinates : occupiedCellsOfOneType){
+            int maxConsecutiveLengthForCell = winChecker.maxConsecutiveLengthWithDirections(coordinates[0], coordinates[1])[0];
+            if (maxConsecutiveLength < maxConsecutiveLengthForCell){
+                maxConsecutiveLength = maxConsecutiveLengthForCell;
+                cellCoordinatesWithMaxConsecutiveLength = coordinates;
+            }
+        }
+        return cellCoordinatesWithMaxConsecutiveLength;
+    }
 
-                if (boardArray[i][j] == cellState) {
-                    int max = winChecker.maxConsecutiveLengthWithDirections(i, j)[0];
-                    int max_dx = winChecker.maxConsecutiveLengthWithDirections(i, j)[1];
-                    int max_dy = winChecker.maxConsecutiveLengthWithDirections(i, j)[2];
-                        x = i + (max-1) * max_dx;
-                        y = j + (max-1) * max_dy;
-                    continue;
-                }
+    public int[] cellDecision(Board board){
+        CellState opponentCellState = null;
+        for (CellState cellType : CellState.values()){
+            if (cellType != cellState && cellType != CellState.EMPTY){
+                opponentCellState = cellType;
+            }
+        }
+        int[] opponentCellWithMaxLength = getCellWithHighestConsecutiveLength(board, opponentCellState);
+        int[] myCellWithMaxLength = getCellWithHighestConsecutiveLength(board, cellState);
+        int opponentMaxLength = winChecker.maxConsecutiveLengthWithDirections(opponentCellWithMaxLength[0], opponentCellWithMaxLength[1])[0];
+        int myMaxLength = winChecker.maxConsecutiveLengthWithDirections(myCellWithMaxLength[0], myCellWithMaxLength[1])[0];
+        if (opponentMaxLength >= myMaxLength){
+            return opponentCellWithMaxLength;
+        } else {
+            return myCellWithMaxLength;
+        }
+        //TO DO: improve logic. misses threats.
+    }
 
-                if (boardArray[i][j] == CellState.EMPTY) {
-                    Random r = new Random();
-                    x = r.nextInt(board.boardSize-1);
-                    y = r.nextInt(board.boardSize-1);
-                    continue;
-                }
-
-
-
+    @Override
+    public int[] generateCoordinates(Board board){
+        int i = cellDecision(board)[0];
+        int j = cellDecision(board)[1];
+        int max = winChecker.maxConsecutiveLengthWithDirections(i, j)[0];
+        int max_dx = winChecker.maxConsecutiveLengthWithDirections(i, j)[1];
+        int max_dy = winChecker.maxConsecutiveLengthWithDirections(i, j)[2];
+        int x = i + max * max_dx;
+        int y = j + max * max_dy;
+        if (!board.checkIfCellEmpty(x,y)){
+            Random r = new Random();
+            while(!board.checkIfCellEmpty(x,y)){
+                x = r.nextInt(board.boardSize-1);
+                y = r.nextInt(board.boardSize-1);
             }
         }
         return new int[] {x,y};
